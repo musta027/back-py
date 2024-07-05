0dab9f08d43ec8446e73774177a54f57e997f648
musta027 <kassymoff01@gmail.com>
2024-07-05 09:55:18 +0500

    рабочий бэк v2

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ from fpdf import FPDF
 import openai
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-# Загрузка переменных окружения из .env файла
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://epitet.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 if not openai.api_key:
     raise HTTPException(status_code=500, detail="OpenAI API key is not set")
 
 class DocumentRequest(BaseModel):
-    document_type: str
     user_input: str
 
 class PDF(FPDF):
     def header(self):
         self.add_font('DejaVu', '', './fonts/DejaVuSans.ttf', uni=True)
         self.set_font('DejaVu', '', 12)
-        self.cell(0, 10, 'Официальный документ', 0, 1, 'C')
-
-    def chapter_title(self, title):
-        self.set_font('DejaVu', '', 12)
-        self.cell(0, 10, title, 0, 1, 'L')
+        self.cell(0, 10, 'tezDet.ai', 0, 1, 'C')
 
     def chapter_body(self, body):
         self.set_font('DejaVu', '', 12)
         self.multi_cell(0, 10, body)
+        self.ln()
 
 @app.post("/generate_document/")
 async def generate_document(request: DocumentRequest):
@@ -39,17 +44,16 @@ async def generate_document(request: DocumentRequest):
         response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": f"Создай официальный документ типа {request.document_type} на основе следующих данных: {request.user_input}"}
+                {"role": "system", "content": "You are a personal lawyer, attorney, notary, accountant, secretary, office manager, administrative staff, HR specialist, and contract manager. Your job is to quickly, accurately, safely, and error-free fill out necessary documents, forms, and so on. Generate only the main content of the document without any additional notes or warnings."},
+                {"role": "user", "content": f"На основе следующих данных: {request.user_input}, составь основной текст документа. Не включай никакие предупреждения, примечания или лишние объяснения."}
             ],
             max_tokens=1000
         )
         document_text = response['choices'][0]['message']['content'].strip()
 
         # Создание PDF документа
-        pdf = PDF()
+        pdf = PDF(format='A4')
         pdf.add_page()
-        pdf.chapter_title(f"Тип документа: {request.document_type}")
         pdf.chapter_body(document_text)
 
         # Сохранение PDF в файл
